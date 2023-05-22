79330f8b0dba6cb59d2d90a22886e29c1e265dbf
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-05-22 14:51:20 +0200

    create new file 'homeController' and add function

---
+++ src/controller/homeController.js
@@ -0,0 +1,35 @@
+/**
+ * Création de la route : GET qui me retourne une chaîne de caractère
+ * "Salut la compagnie !"
+ * ⬇️
+ */
+const getWelcome = (req, res) => {
+	// Ici, le console.log n'est disponible QUE pour le backend
+	// console.log("Salut la compagnie !");
+	// res.send renvoi une donnée vers le périphérique qui l'a appelé
+	res.json({
+		message: "Salut la compagnie !",
+	});
+};
+const postWelcome = (req, res) => {
+	res.json({
+		message: "Je suis nouvelle !",
+	});
+};
+const putWelcome = (req, res) => {
+	res.json({
+		message: "Je me met à jour !",
+	});
+};
+const deleteWelcome = (req, res) => {
+	res.json({
+		message: "C'est ciao !",
+	});
+};
+
+module.exports = {
+	getWelcome,
+	putWelcome,
+	postWelcome,
+	deleteWelcome,
+};
--- src/index.js
+++ src/index.js
@@ -2,43 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 5500;
 
-/**
- * Création de la route : GET qui me retourne une chaîne de caractère
- * "Salut la compagnie !"
- * ⬇️
- */
-const getWelcome = (req, res) => {
-	// Ici, le console.log n'est disponible QUE pour le backend
-	// console.log("Salut la compagnie !");
-	// res.send renvoi une donnée vers le périphérique qui l'a appelé
-	res.json({
-		message: "Salut la compagnie !",
-	});
-};
-const postWelcome = (req, res) => {
-	res.json({
-		message: "Je suis nouveau !",
-	});
-};
-const putWelcome = (req, res) => {
-	res.json({
-		message: "Je me met à jour !",
-	});
-};
-const deleteWelcome = (req, res) => {
-	res.json({
-		message: "C'est ciao !",
-	});
-};
+const homeController = require("./controller/homeController");
 
 // .get(la route, callback )
 // Pour tester ma route, je vais sur postman / insomnia / navigateur (uniquement pour le get)
 // avec l'url suivante POUR MON CAS
 // http://localhost:5500
-app.post("/", postWelcome);
-app.get("/", getWelcome);
-app.put("/", putWelcome);
-app.delete("/", deleteWelcome);
+app.post("/", homeController.postWelcome);
+app.get("/", homeController.getWelcome);
+app.put("/", homeController.putWelcome);
+app.delete("/", homeController.deleteWelcome);
 
 //         PORT, CALLBACK = Une fonction dans une fonction
 app.listen(PORT, (err) => {
