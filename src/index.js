70faf76cba24ba4bd9779d529e30d627a794b201
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-05-22 14:41:00 +0200

    add first route : GET, now POST, PUT, DELETE

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 5500;
 
+/**
+ * Création de la route : GET qui me retourne une chaîne de caractère
+ * "Salut la compagnie !"
+ * ⬇️
+ */
+const getWelcome = (req, res) => {
+	// Ici, le console.log n'est disponible QUE pour le backend
+	// console.log("Salut la compagnie !");
+	// res.send renvoi une donnée vers le périphérique qui l'a appelé
+	res.json({
+		message: "Salut la compagnie !",
+	});
+};
+
+// .get(la route, callback )
+// Pour tester ma route, je vais sur postman / insomnia / navigateur (uniquement pour le get)
+// avec l'url suivante POUR MON CAS
+// http://localhost:5500
+app.get("/", getWelcome);
+
 //         PORT, CALLBACK = Une fonction dans une fonction
 app.listen(PORT, (err) => {
 	if (err) {
