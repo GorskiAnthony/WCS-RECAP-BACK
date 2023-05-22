431b9ba68647c2c6fef550a84135d245341982f8
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-05-22 14:26:09 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-
 const app = express();
+const PORT = 5500;
 
-app.listen(5000, () => {
-	console.log("Je suis run sur le port: 5000");
+//         PORT, CALLBACK = Une fonction dans une fonction
+app.listen(PORT, (err) => {
+	if (err) {
+		console.log(`Oups, une erreur s'est produite, l'erreur : ${err}`);
+	} else {
+		console.log(`Je suis run sur le port: ${PORT}`);
+	}
 });
